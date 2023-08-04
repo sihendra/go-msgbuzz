9927474f25d1031416cce2f41545ac574e6049d1
Hendra Setiawan <hendra.setiawan@shortlyst.ai>
2023-08-04 17:04:39 +0700

    Improve readability of RabbitMqClient Publish integration test

--- rabbitmq_integration_test.go
+++ rabbitmq_integration_test.go
@@ -1,3 +1,4 @@
+//go:build integration
 // +build integration
 
 package msgbuzz
@@ -10,81 +11,84 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestRabbitMqClient(t *testing.T) {
-	// Init
-	rabbitClient := NewRabbitMqClient(os.Getenv("RABBITMQ_URL"), 1)
-	testTopicName := "msgbuzz.pubtest"
-	actualMsgSent := make(chan bool)
+func TestRabbitMqClient_Publish(t *testing.T) {
 
-	// Code under test
-	rabbitClient.On(testTopicName, "msgbuzz", func(confirm MessageConfirm, bytes []byte) error {
-		t.Logf("Receive msg from topic %s", testTopicName)
-		actualMsgSent <- true
-		return confirm.Ack()
+	t.Run("ShouldPublishMessageToTopic", func(t *testing.T) {
+		// Init
+		rabbitClient := NewRabbitMqClient(os.Getenv("RABBITMQ_URL"), 1)
+		testTopicName := "msgbuzz.pubtest"
+		actualMsgReceivedChan := make(chan []byte)
+
+		// -- listen topic to check published message
+		rabbitClient.On(testTopicName, "msgbuzz", func(confirm MessageConfirm, bytes []byte) error {
+			actualMsgReceivedChan <- bytes
+			return confirm.Ack()
+		})
+		go rabbitClient.StartConsuming()
+		defer rabbitClient.Close()
+
+		// -- wait for exchange and queue to be created
+		time.Sleep(3 * time.Second)
+
+		// Code under test
+		sentMessage := []byte("some msg from msgbuzz")
+		err := rabbitClient.Publish(testTopicName, sentMessage)
+
+		// Expectations
+		// -- ShouldPublishMessageToTopic
+		require.NoError(t, err)
+
+		// -- Should receive correct msg
+		waitSec := 20
+		select {
+		case <-time.After(time.Duration(waitSec) * time.Second):
+			t.Fatalf("Not receiving msg after %d seconds", waitSec)
+		case actualMessageReceived := <-actualMsgReceivedChan:
+			require.Equal(t, sentMessage, actualMessageReceived)
+		}
 	})
-	go rabbitClient.StartConsuming()
-	defer rabbitClient.Close()
 
-	// wait for exchange and queue created
-	time.Sleep(3 * time.Second)
+	t.Run("ShouldReconnectAndPublishToTopic_WhenDisconnectedFromRabbitMqServer", func(t *testing.T) {
+		// Init
+		err := StartRabbitMqServer()
+		require.NoError(t, err)
 
-	err := rabbitClient.Publish(testTopicName, []byte("some msg from msgbuzz"))
+		rabbitClient := NewRabbitMqClient(os.Getenv("RABBITMQ_URL"), 1)
+		rabbitClient.SetRcStepTime(1)
+		topicName := "msgbuzz.reconnect.test"
+		consumerName := "msgbuzz"
+		actualMsgSent := make(chan bool)
 
-	// Expectations
-	// -- Should publish msg
-	require.NoError(t, err)
+		// Code under test
+		rabbitClient.On(topicName, consumerName, func(confirm MessageConfirm, bytes []byte) error {
+			t.Logf("Receive message from topic %s", topicName)
+			actualMsgSent <- true
+			return confirm.Ack()
+		})
+		go rabbitClient.StartConsuming()
+		defer rabbitClient.Close()
 
-	// -- Should receive msg
-	waitSec := 20
-	select {
-	case <-time.After(time.Duration(waitSec) * time.Second):
-		t.Fatalf("Not receiving msg after %d seconds", waitSec)
-	case msgSent := <-actualMsgSent:
-		require.True(t, msgSent)
-	}
+		// wait for exchange and queue to be created
+		time.Sleep(500 * time.Millisecond)
 
-}
+		// restart RabbitMQ dummy server
+		err = RestartRabbitMqServer()
+		require.NoError(t, err)
 
-func TestRabbitMqClient_ShouldReconnectAndPublishToTopic_WhenDisconnectFromRabbitMqServer(t *testing.T) {
-	// Init
-	err := StartRabbitMqServer()
-	require.NoError(t, err)
+		err = rabbitClient.Publish(topicName, []byte("Hi from msgbuzz"))
 
-	rabbitClient := NewRabbitMqClient(os.Getenv("RABBITMQ_URL"), 1)
-	rabbitClient.SetRcStepTime(1)
-	topicName := "msgbuzz.reconnect.test"
-	consumerName := "msgbuzz"
-	actualMsgSent := make(chan bool)
+		// Expectations
+		// -- Should publish message
+		require.NoError(t, err)
 
-	// Code under test
-	rabbitClient.On(topicName, consumerName, func(confirm MessageConfirm, bytes []byte) error {
-		t.Logf("Receive message from topic %s", topicName)
-		actualMsgSent <- true
-		return confirm.Ack()
+		// -- Should receive message
+		waitSec := 20
+		select {
+		case <-time.After(time.Duration(waitSec) * time.Second):
+			t.Fatalf("Not receiving message after %d seconds", waitSec)
+		case msgSent := <-actualMsgSent:
+			require.True(t, msgSent)
+		}
 	})
-	go rabbitClient.StartConsuming()
-	defer rabbitClient.Close()
-
-	// wait for exchange and queue to be created
-	time.Sleep(500 * time.Millisecond)
-
-	// restart RabbitMQ dummy server
-	err = RestartRabbitMqServer()
-	require.NoError(t, err)
-
-	err = rabbitClient.Publish(topicName, []byte("Hi from msgbuzz"))
-
-	// Expectations
-	// -- Should publish message
-	require.NoError(t, err)
-
-	// -- Should receive message
-	waitSec := 20
-	select {
-	case <-time.After(time.Duration(waitSec) * time.Second):
-		t.Fatalf("Not receiving message after %d seconds", waitSec)
-	case msgSent := <-actualMsgSent:
-		require.True(t, msgSent)
-	}
 
 }
